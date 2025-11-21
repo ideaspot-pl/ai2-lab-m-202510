ec5f7838e9845bfe321637a131bd6484fdb1df6b
Artur Karczmarczyk <artur@ideaspot.pl>
2025-11-21 13:16:21 +0100

    adding tasks

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,16 +1,20 @@
 import {Component, OnInit} from '@angular/core';
 import {Task} from '../task';
 import {Tasks as TasksService} from '../tasks';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
 export class Tasks implements OnInit {
-  tasks: Task[] = [];
-  newTask: Task = {};
+  public tasks: Task[] = [];
+  public newTask: Task = {};
+  public isProcessing = false;
 
   constructor(
     private tasksService: TasksService,
@@ -20,6 +24,24 @@ export class Tasks implements OnInit {
   ngOnInit(): void {
     this.tasksService.index().subscribe((tasks) => {
       this.tasks = tasks;
+    });
+    this.isProcessing = false;
+  }
+
+  addTask() {
+    if (this.newTask.title === undefined) {
+      return;
+    }
+
+    this.newTask.completed = false;
+    this.newTask.archived = false;
+
+    this.tasks.unshift(this.newTask); // optimistic update; try commenting this line off and compare the difference
+
+    this.isProcessing = true;
+    this.tasksService.post(this.newTask).subscribe((task) => {
+      this.newTask = {};
+      this.ngOnInit();
     });
   }
 }

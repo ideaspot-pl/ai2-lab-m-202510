16997138b47c94cb4546ac4dfd990922921aaf14
Artur Karczmarczyk <artur@ideaspot.pl>
2025-11-23 19:29:55 +0100

    header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatChipListbox, MatChipOption} from '@angular/material/chips';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatChipListbox, MatChipOption, MatToolbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
